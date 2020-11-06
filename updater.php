9098b40dd6d2409d43374facdab6e96fb018b440
Max Base <MaxBaseCode@Gmail.Com>
2020-11-06 17:02:54 +0330

    Update updater.php

--- updater.php
+++ updater.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * @Author: Max Base
- * @Repository: https://github.com/BaseMax/GooglePlayDatabaseMirror/new/main
- * @Date: 2020-10-29
+ * @Repository: https://github.com/BaseMax/GooglePlayDatabaseMirror
+ * @Date: 2020-10-29, 2020-11-06
 */
 
 require "google-play.php";
@@ -14,30 +14,30 @@ $db->db="phpcrawler";
 $db->create_database($db->db,false);
 
 function insertApp($app) {
-global $db;
-if($app == []) {
-return;
-}
-$images=$app["images"];
-unset($app["images"]);
-$clauses=["packageName"=>$app["packageName"]];
-print_r($app);
-if($db->count("app", $clauses) == 0) {
-$appID=$db->insert("app", $app);
-}
-else {
-$db->update("app", $clauses, $app);
-$app=$db->select("app", $clauses, "", "id");
-$appID=$app["id"];
-}
-if($images !== []) {
-foreach($images as $image) {
-$clauses=["appID"=>$appID, "image"=>$image];
-if($db->count("screen", $clauses) == 0) {
-$db->insert("screen", $clauses);
-}
-}
-}
+    global $db;
+    if($app == []) {
+        return;
+    }
+    $images=$app["images"];
+    unset($app["images"]);
+    $clauses=["packageName"=>$app["packageName"]];
+    print_r($app);
+    if($db->count("app", $clauses) == 0) {
+        $appID=$db->insert("app", $app);
+    }
+    else {
+        $db->update("app", $clauses, $app);
+        $app=$db->select("app", $clauses, "", "id");
+        $appID=$app["id"];
+    }
+    if($images !== []) {
+        foreach($images as $image) {
+        $clauses=["appID"=>$appID, "image"=>$image];
+            if($db->count("screen", $clauses) == 0) {
+                $db->insert("screen", $clauses);
+            }
+        }
+    }
 }
 
 function insertApps($apps) {
